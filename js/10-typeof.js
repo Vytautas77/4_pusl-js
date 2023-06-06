84b5c4e5850f8e667a9a3d381ccb40e8b5799697
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-06-06 11:17:27 +0300

    Typeof and arrayis

---
+++ js/10-typeof.js
@@ -0,0 +1,20 @@
+console.clear();
+/*
+TYPEOF (tipo nustatymas) naudingas su primitiviomis reiksmemeis
+operatorius
+(Array.isArray(5))
+*/
+
+console.log(typeof 5);
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(typeof '');
+console.log(typeof true);
+console.log(typeof []);
+console.log(typeof {})
+
+console.log('~~~~~~~~~~~~');
+
+console.log (Array.isArray(5));
+console.log (Array.isArray ({}));
+console.log (Array.isArray([])); //masyvas
