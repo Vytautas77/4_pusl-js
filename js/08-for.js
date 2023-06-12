44c6bbb29df2b72487a074a4b7c3111af2b7b6a9
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-06-12 16:49:12 +0300

    function

--- js/08-for.js
+++ js/08-for.js
@@ -7,34 +7,47 @@ for (pradzia; klausimas; pakeitimas) {
 */
 
 const marks = [10, 2, 8, 4, 6];
-let suma = 0;
-let index = 0;
-suma += marks[index++];
-suma += marks[index++];
-suma += marks[index++];
-suma += marks[index++];
-suma += marks[index++];
-console.log( suma/marks.length);
-console.log('~~~~~~~~~~');
+let sum = 0;
+let index =0;
+sum += marks[0];
+sum += marks[1];
+sum += marks[2];
+sum += marks[3];
+sum += marks[4];
+console.log ('vidurkis', sum / marks.length);
 
-const pazymiai = [10, 2, 8, 4, 6, 4, 10, 34];
-let sma = 0;
-for (let i=0; i<pazymiai.length; i++) {
-    console.log(i, '-->', pazymiai[i])}
-
-/* for (let i=0; i < 5; i++) {
-    console.log(i)
-}*/
-console.log('~~~~~~~~~~');
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i=0; i <numbers.length; i+=3)  {
-    console.log (i, '-->', numbers[i]);
+// Isspaisdinti nuo 0 iki 5.
+for (let i=0; i<=5; i++){
+    console.log(i);
 }
-console.log('~~~~~~~~~~');
-// Sveiki, mano vardas yra [Vardas]!
-const studens = ['Petras', 'Maryte', 'Jonas', 'Ona'];
-for (let i=0; i<studens.length; i++) {
-    const name = studens[i];
+// antras pvz
+console.log ('Antras pvz ~~~~~~~~~~~')
+
+const pazymiai = [2, 4, 6, 8, 10, 8, 6, 9, 7];
+let sums =0;
+    for (let i=1; i<pazymiai.length; i++) {
+        console.log(i, '-->', pazymiai[i]);
+    }
+// trecias pvz
+console.log ('trecias pvz ~~~~~~~~~~~')
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numbers.length; i+=3) {
+    console.log (numbers[i]);
+}
+// ketvirtas pvz
+console.log ('su String pvz ~~~~~~~~~~~')
+const abc = 'abcdefgh';
+let text = '';
+for (let i = 0; i< abc.length; i++){
+    text += abc[i];
+    console.log (i, abc [i], text);
+}
+// ketvirtas pvz
+console.log ('ketvirtas pvz ~~~~~~~~~~~')
+const students = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+// Sveiki, mano vardas yra [vardas]
+for (i=0; i< students.length; i++){
+    const name = students[i];
     const hi = `Sveiki, mano vardas yra ${name}!`;
     console.log (hi);
 }
--- js/09-function.js
+++ js/09-function.js
@@ -2,70 +2,42 @@ console.clear();
 /*
 function sudetis(a, b) {
     return a+b;
+}*/
+
+function sudetis (a, b) {
+    return a+b;
 }
-const s1 = sudetis(7, 5);
-console.log(`7+5=${s1}`);
+const s1 = sudetis (7, 5);
+console.log (`7 + 5 = ${s1}`);
 
-const s2 = sudetis(21, -7);
-console.log(`21-7=${s2}`);
+console.log ('~~~~~~~ Ar lyginis ~~~~~~~~~~~~');
 
-function arLyginis(skaicius){
-    if (skaicius %2 ===0) {
+function arLyginis (skaicius){
+    if (skaicius % 2 == 0){
         return 'lyginis'
     }
-    return 'ne lyginis'
+    return 'ne lyginis';
 }
-const l1=arLyginis(7);
-console.log(`7 yra ${l1}`)
+const l1 = arLyginis(7);
+console.log (`7 yra ${l1} `);
 
-const l2=arLyginis(88);
-console.log(`88 yra ${l2}`)
+const l2 = arLyginis(88);
+console.log (`88 yra ${l2} `);
 
-console.log ('~~~~~~~~~~~~~');
+console.log ('~~~~~~~ su pazymiai ~~~~~~~~~~~~');
+
 const petras = [10, 2, 8, 4, 6];
 const maryte = [10, 9, 8, 7];
 const jonas = [2, 3, 4, 5];
-const ona = [5, 5, 5, 5, 5, 5, 5, 5, 5];
+const ona = [5, 5, 5, 5, 5, 5];
 
-function average(marks) {
-   let sum = 0;
-   for (let i=0; i<marks.length; i++){
-    sum +=marks[i]
-   }
-    return sum / marks.length;
-}
-console.log('Petras vidurkis', average(petras), petras);
-console.log('Maryte vidurkis', average(maryte), maryte);
-console.log('Jonas vidurkis', average(jonas), jonas);
-console.log('Ona vidurkis', average(ona), ona); 
-
-console.log ('~~~~~~~~~~~~~~~~~~~~');
-
-function vidurkis(skaicius) {
-    if (skaicius%2 === 0){
-        return 'lyginis'
-    }
-    return 'nelyginis'   
-}
-const l1 = vidurkis (88);
-    console.log (`Skaicius 88 yra ${l1}`);
-const l2 = vidurkis (77);
-    console.log (`Skaicius 77 yra ${l2}`);
-    */
-    console.log ('~~~~~~~~~~~~~~~~~~~~');
-    function skaic (a, b) {
-        //jei a yra ne skaicius
-        if (typeof a!== 'number'){
-            return 'Klaida: pirmas parametras yra ne skaicius';
+function average (marks) {
+    let sum = 0;
+    for (i=0; i< marks.length; i++)
+    sum += marks[i];
+          return sum/marks.length;
         }
-        //jei b yra ne skaicius
-            if (typeof b!== 'number'){
-                return 'Klaida: antras parametras yra ne skaicius'
-            }
-        return a-b;        
-    }
-    console.log (`Kaiciu suma lygi ${skaic(5,7)}`);
-    console.log (`Kaiciu suma lygi ${skaic(true,7)}`);
-    console.log (`Kaiciu suma lygi ${skaic(5,true)}`);
-    console.log (`Kaiciu suma lygi ${skaic(false,7)}`);
-    console.log (`Kaiciu suma lygi ${skaic(5,false)}`);
+console.log('Petras:', average(petras), petras);
+console.log('Maryte:', average(maryte), maryte);
+console.log('Jonas:', average(jonas), jonas);
+console.log('Ona:', average(ona), ona);
