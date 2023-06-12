201513e3b24f558ed374befd7c9d9aaad13616b8
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-06-12 12:23:44 +0300

    swicth

--- js/07-switch.js
+++ js/07-switch.js
@@ -8,64 +8,79 @@ switch (key) {
 
     default:
         break;
-}
+}*/
 
-*/
-const diena = 2;
-
+const diena =10;
 switch (diena) {
     case 1:
-        console.log('pirmadienis');        
+        console.log('pirmadienis');
+    break;
+    case 2:
+        console.log('antradiens');
         break;
-        case 2:
-        console.log('antradienis');        
+    case 3:
+        console.log('treciadienis');
         break;
-
+    case 4:
+        console.log('ketvirtadiens');
+        break;
+    case 5:
+        console.log('penktadienis');
+        break;
+    case 6:
+        console.log('sestadiens');
+        break;
+    case 7:
+        console.log('sekmadienis');
+    break;
     default:
-        console.log('tokios dienos savaiteje nera')
-        break;
+        console.log("tokios dienos savaiteje nerra!")
+    break;
 }
-console.log('~~~~~~~~~~~');
-const day = 6;
+// antras variantas
+const day =8;
 switch (day) {
     case 1:
     case 2:
     case 3:
     case 4:
     case 5:
-        console.log('darbo diena');        
+        console.log("darbo diena");
         break;
     case 6:
     case 7:
-        console.log('savaitgalis');        
+        console.log('savaitgals');
         break;
     default:
-        console.log('tokios dienos savaiteje nera')
+        console.log('tokios dienos nera !')
         break;
 }
-console.log ('~~~~~~~ pvz su IF~~~~~');
-if (day ===1 || day ===2 || day ===3 || day ===4 || day ===5){
-    console.log('darbo diena');  
-}else if (day ===6 || day ===7){
-    console.log('savaitgalis'); 
-} else {
-    console.log('tokios dienos savaiteje nera');
-}
-console.log('~~panaikinus BREAK renkam4s is ko liko~~~~~~~~~~');
-const gamybosEtapas = 3;
+// tas pats su IF
+         if (day=== 1 || day === 2 || day === 3 || day === 4 || day === 5) {
+            console.log("darbo diena");
+         } else if (day === 6 || day == 7) {
+            console.log('savaitgals');      
+         } else {
+            console.log('tokios dienos nera !')
+         }
+// trecias variantas eiles tvarka
+console.log('~~~~~~~~~~~~~~')
+const gamybosEtapas = 5;
 switch (gamybosEtapas) {
     case 1:
-        console.log('Pasiimti puodeli');        
+        console.log ('pasiimti puodeli');
     case 2:
-        console.log('isideti arbatos');        
+        console.log ('isideti ardatos');
     case 3:
-        console.log('pasisaldinti jei norisi');        
+        console.log ('pasisaldinti arba ne');
     case 4:
-        console.log('Isipilti vyrinto vandens');        
+        console.log ('uzsipilti vyrinto vandens');
     case 5:
-    console.log('Gerti');        
-    break;
+        console.log ('atsiferti arbatos');
+    case 6:
+        console.log ('eiti dirbti');
+        break;
     default:
-        console.log ('Tokio etapo nera');
+        console.log('Tokio etapo nera');
         break;
 }
